ca51e3dd93e96d252c3b526356e6a1a0881109ae
unknown <majoca10@hotmail.com>
2021-09-29 12:28:24 -0500

    validaciones add

--- pages/ubicacion/pais/index.vue
+++ pages/ubicacion/pais/index.vue
@@ -9,7 +9,7 @@
     justify="center" 
     style="height:100vh" dense
   >
-    <v-colm style="height: 100%; width: 45%; margin-top: 25px; padding-right: 15px;">
+    <v-colm style="height: 100%; width: 30%; margin-top: 25px; padding-right: 15px;">
       <v-card elevation="3" aling="center">
         <v-card-title>Paises</v-card-title>
         <v-card-text>Test de informacion de datos de paises</v-card-text>
@@ -17,52 +17,53 @@
             <v-text-field
               v-model="name"
               :error-messages="nameErrors"
-              :counter="10"
-              label="Name"
+              :counter="4"
+              label="Pais"
               required
               @input="$v.name.$touch()"
               @blur="$v.name.$touch()"
             ></v-text-field>
             <v-text-field
-              v-model="email"
-              :error-messages="emailErrors"
-              label="E-mail"
+              v-model="codigo"
+              :error-messages="codigoErrors"
+              label="Codigo"
               required
-              @input="$v.email.$touch()"
-              @blur="$v.email.$touch()"
+              @input="$v.codigo.$touch()"
+              @blur="$v.codigo.$touch()"
+              type="number"
             ></v-text-field>
-            <v-select
-              v-model="select"
-              :items="items"
-              :error-messages="selectErrors"
-              label="Item"
+              <v-text-field
+              v-model="codigo2"
+              :error-messages="codigo2Errors"
+              label="Codigo2"
               required
-              @change="$v.select.$touch()"
-              @blur="$v.select.$touch()"
-            ></v-select>
-            <v-checkbox
-              v-model="checkbox"
-              :error-messages="checkboxErrors"
-              label="Do you agree?"
+              @input="$v.codigo2.$touch()"
+              @blur="$v.codigo2.$touch()"
+              type="number"
+            ></v-text-field>
+            <v-text-field
+              v-model="codigofe"
+              :error-messages="codigofeErrors"
+              label="Codigo Factura Electronica"
               required
-              @change="$v.checkbox.$touch()"
-              @blur="$v.checkbox.$touch()"
-            ></v-checkbox>
-
+              @input="$v.codigofe.$touch()"
+              @blur="$v.codigofe.$touch()"
+              type="number"
+            ></v-text-field>
             <v-btn
               class="mr-4"
-              @click="submit"
+              @click="agregar()"
               color="success"
             >
               Agregar
             </v-btn>
-            <v-btn @click="clear">
+            <v-btn @click="limpiar()">
               Limpiar
             </v-btn>
         </form>
       </v-card>
       </v-colm>
-        <v-colm style="height: 100%; width: 45%; margin-top: 25px;">
+        <v-colm style="height: 100%; width: 60%; margin-top: 25px;">
         <v-card elevation="3">
         <v-card-title>Paises</v-card-title>
         <v-card-text>Test de informacion de datos de paises</v-card-text>
@@ -87,6 +88,8 @@
                       <th class="text-left">
                         Codigo
                       </th>
+                      <th>Codigo2</th>
+                      <th>Codigo FE</th>
                       <th></th>
                       <th></th>
                     </tr>
@@ -99,11 +102,13 @@
                       <td>{{ item.id }}</td>
                       <td>{{ item.pais }}</td>
                       <td>{{ item.codigo }}</td>
-                      <td><v-btn tile color="success">
+                      <td>{{ item.codigo2 }}</td>
+                      <td>{{ item.codigofe }}</td>
+                      <td><v-btn tile color="success" @click="edit(item.id,item.pais,item.codigo,item.codigo2,item.codigofe)">
                       <v-icon left>mdi-pencil</v-icon>
                       Edit
                       </v-btn></td>
-                      <td><v-btn tile color="error">
+                      <td><v-btn tile color="error" @click="eliminar(item.id)">
                       <v-icon left>mdi-minus</v-icon>
                       Edit
                       </v-btn></td>
@@ -122,76 +127,125 @@
 <script>
   import { validationMixin } from 'vuelidate'
   import { required, maxLength, email } from 'vuelidate/lib/validators'
-
+  
   export default {
     mixins: [validationMixin],
 
     validations: {
-      name: { required, maxLength: maxLength(10) },
-      email: { required, email },
-      select: { required },
-      checkbox: {
-        checked (val) {
-          return val
-        },
-      },
+      name: { required, maxLength: maxLength(4) },
+      codigo: { required },
+      codigo2: { required },
+      codigofe: { required },
     },
   
     data (){
       return{
-        name: '',
-        email: '',
-        select: null,
         search:'',
         paises:[
           {
-            id: '1',  
-            pais: 'Colombia',
-            codigo: 169,
+            id: '',  
+            pais: '',
+            codigo: '',
+            codigo2:'',
+            codigofe:''
           },
         ]
       }
     },
+
+    created(){
+
+      this.mostrar()
+
+    },
+  methods:{
+
+      mostrar(){
+        axios.get(baseURL).then(response=>{
+          this.paises=response.data;
+        })
+      },
+
+      crear(){
+        let parametros = {pais:this.paises.name, codigo:this.paises.codigo, codigo:this.paises.codigo2, codigo:this.paises.codigofe }
+        axios.post(baseURL, parametros).then(response=>{
+          this.mostrar()
+        })
+        this.paises.name='';
+        this.paises.codigo='';
+        this.paises.codigo2='';
+        this.paises.codigofe='';
+
+      },
+
+      editar(){
+        let parametros = {id:this.paises.id, pais:this.paises.name, codigo:this.paises.codigo, codigo:this.paises.codigo2, codigo:this.paises.codigofe }
+        axios.put(baseURL+this.paises.id, parametros).then(response=>{
+          this.mostrar()
+        })  
+
+      },
+
+      eliminar(){
+        Swal.fire({
+          title:'Confirma la eliminacion del registro',
+          confirmButtonText:'Confirmar',
+          showCancelButton:true,
+        }).then((result)=>{
+          if(result.isConfirmed){
+            axios.delete(baseURL+id).the(response=>{
+              this.mostrar()
+            });
+            Swal.fire('Eliminado','','success')
+          }else if(result.isDenied){}
+        });
+      },
+
+    },
     
     computed: {
-      checkboxErrors () {
-        const errors = []
-        if (!this.$v.checkbox.$dirty) return errors
-        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-        return errors
-      },
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
+
       nameErrors () {
         const errors = []
         if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
+        !this.$v.name.maxLength && errors.push('Pais Requiere almenos 4 Caracteres')
+        !this.$v.name.required && errors.push('Pais es Requerido')
         return errors
       },
-      emailErrors () {
+      codigoErrors () {
         const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
+        if (!this.$v.codigo.$dirty) return errors
+        !this.$v.codigo.maxLength && errors.push('Codigo Requiere almenos 1 Caracteres')
+        !this.$v.codigo.required && errors.push('Codigo es Requerido')
         return errors
       },
+      codigo2Errors () {
+        const errors = []
+        if (!this.$v.codigo2.$dirty) return errors
+        !this.$v.codigo2.maxLength && errors.push('Codigo2 Requiere almenos 1 Caracteres')
+        !this.$v.codigo2.required && errors.push('Codigo2 es Requerido')
+        return errors
+      },
+      codigofeErrors () {
+        const errors = []
+        if (!this.$v.codigofe.$dirty) return errors
+        !this.$v.codigofe.maxLength && errors.push('CodigoFE Requiere almenos 1 Caracteres')
+        !this.$v.codigofe.required && errors.push('CodigoFE es Requerido')
+        return errors
+      },
+
     },
 
     methods: {
-      submit () {
+      agregar () {
         this.$v.$touch()
       },
-      clear () {
+      limpiar () {
         this.$v.$reset()
         this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
+        this.codigo = ''
+        this.codigo2 = ''
+        this.codigofe = ''
       },
     },
 
