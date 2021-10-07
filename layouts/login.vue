dd2c4d1a50ee67fbc0b381dd46cb83d97ad4583e
majoca10 <majoca10@hotmail.com>
2021-10-07 17:56:45 -0500

    sincelejo

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,55 +1,50 @@
 <template>
-      <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title class="pb-0">
-        <h1>Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field 
-            label="Usuario" 
-            prepend-icon="mdi-account-circle"
-            v-model="login.usuario"
-          />
-          <v-text-field 
-            label="Password"
-            prepend-icon="mdi-lock"
-            v-model="login.password"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="info" @click="login()">Ingresar</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-app>
+  <div>
+    <form @submit.prevent="login">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+          type="text"
+          class="form-control"
+          id="email"
+          v-model="loginData.usuario"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input
+          type="password"
+          v-model="loginData.password"
+          class="form-control"
+          id="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary w-100">login</button>
+    </form>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      usuario: '',
-      password: '',
-      error: null
-    }
+      loginData: {
+        usuario: "",
+        password: ""
+      }
+    };
   },
-
   methods: {
     async login() {
       try {
-        await this.$auth.post('local', {
-          data: {
-          usuario: this.usuario,
-          password: this.password
-          }
-        })
-            console.log(data)
-        this.$router.push('/board')
-      } catch (e) {
-        this.error = e.response.data.message
+        let response = await this.$auth.loginWith("local", {
+          data: this.loginData
+        });
+        this.$router.push("/");
+        console.log(response);
+      } catch (err) {
+        console.log(err);
       }
     }
   }
-}
+};
 </script>
